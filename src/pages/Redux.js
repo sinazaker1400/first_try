587aec3096e1ecb77105f86889b65fdf15830b13
sinazaker1400 <sinazaker1370@yahoo.com>
2022-06-29 00:57:06 +0430

    some changes in wife manager & adding middleWare

--- src/pages/Redux.js
+++ src/pages/Redux.js
@@ -1,25 +1,55 @@
-import { createStore } from 'redux';
+import { createStore  , applyMiddleware} from 'redux';
 import Button from "./Button";
 import React, { useState } from "react";
 
-const reducer = (state = {
+const wifeReducer = (state = {
   name: 'sina',
   money: 0,
   wives: [],
   work: '',
   age: 0,
+  homes: [],
+  permanentWives: [],
+  children: []
 }, action) => {
   switch (action.type) {
+    case 'PERMANENT_MARRY':
+      if(state.permanentWives.length<4 && state.money>1 && state.homes.length>state.permanentWives.length && state.wives.length>0)
+    {state = {
+      ...state,
+      money: state.money -2,
+      permanentWives: [...state.permanentWives, action.payload],
+      wives: [...state.wives].slice(0,state.wives.length - 1)
+    };}
+  
+    break;
+    case 'HABITATION':
+      state = {
+        ...state,
+        homes: [...state.homes, action.payload],
+        money: state.money -3
+      }
+      break;
+      case 'BORN':
+        if(state.permanentWives.length>0 && state.homes.length>0 && state.money>0)
+      state = {
+        ...state,
+        children: [...state.children, action.payload],
+        money: state.money -1,
+        age: state.age + 0.75
+      };
+      break;
+
     case 'INCOME':
       if(state.work==='architecture'){
       state = {
         ...state,
-        money: state.money + 1,
+        money: state.money + action.payload,
         age: state.age + 1
       }} else if (state.work==='developer'){
         state = {
           ...state,
-          money: state.money + 15,
+          money: state.money + 15*action.payload,
           age: state.age + 1
         }};
       break;
@@ -27,7 +57,7 @@ const reducer = (state = {
         if(state.money>0)
       state = {
         ...state,
-        wives: [...state.wives, action.newWife],
+        wives: [...state.wives, action.payload],
         money: state.money - 1
       };
 
@@ -38,14 +68,16 @@ const reducer = (state = {
         work: action.payload
       };
       break;
-    
   }
   return state;
 };
 
-const store = createStore(reducer);
+const theLogger = (store) => (next) => (action) => {
+console.log('the action is', action);
+next(action);
+};
 
-
+const store = createStore(wifeReducer, applyMiddleware(theLogger));
 
 
 const Wives = () => {
@@ -53,20 +85,37 @@ const Wives = () => {
 
   return (
     <div className="App clearFix">
-      
+
       <p>hi</p>
       <p>you are {state.name}</p>
       <p>you are {state.age} years old</p>
       <p>your work is {state.work}</p>
       <p>you have {state.money} money</p>
-      <p>you have {state.wives.length} wives</p>
-      <p>your wives are {state.wives.map(wife => wife + " ")}</p>
+      <p>you have {state.wives.length} temporary wives</p>
+      <p>your temporary wives are {state.wives.map(wife => wife + " ")}</p>
+      <p>you have {state.permanentWives.length} permanent wives</p>
+      <p>your permanent wives are {state.permanentWives.map(wife => wife + " ")}</p>
+      <p>you have {state.homes.length} homes</p>
+      <p>your homes are {state.homes.map(home => home + " ")}</p>
+      <p>you have {state.children.length} children</p>
+      <p>your children are {state.children.map(child => child+ " ")}</p>
+
+
       <Button 
-      text="add a wife" 
+      text="add a temporary wife" 
       eventHandler={() => {
         store.dispatch({
         type: 'MARRY',
-        newWife: 'marry'
+        payload: 'mary'
+      });
+    setState(store.getState())
+      }}/>
+      <Button 
+      text="add a permanent wife" 
+      eventHandler={() => {
+        store.dispatch({
+        type: 'PERMANENT_MARRY',
+        payload: 'mary'
       });
     setState(store.getState())
       }}/>
@@ -97,13 +146,27 @@ const Wives = () => {
       });
       setState(store.getState())
       }}/>
+      <Button
+      text="have a home" 
+      eventHandler={() => {
+        store.dispatch({
+        type: 'HABITATION', 
+        payload: "home" + `${state.homes.length + 1}`
+      });
+      setState(store.getState())
+      }}/>
+      <Button
+      text="have a child" 
+      eventHandler={() => {
+        store.dispatch({
+        type: 'BORN',
+        payload: "child" + `${state.children.length + 1}`
+      });
+      setState(store.getState())
+      }}/>
+      
       
     </div>
   )
 }
-
-
-
-
-
 export default Wives;
